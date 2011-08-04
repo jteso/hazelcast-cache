b80b87d9757e078740f358a1896374cdd4459376
JTeso <jtejob@gmail.com>
2011-08-04 19:58:22 +1000

    Initial commit

---
+++ bin/startCollect.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+java -Djava.net.preferIPv4Stack=true -cp /usr/local/share/hazelcast-1.9.3.1/lib/hazelcast-1.9.3.1.jar:../out/production/POC com.jteso.poc.superaudit.Collector 40
+
---
+++ bin/startProc.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -Djava.net.preferIPv4Stack=true -cp /usr/local/share/hazelcast-1.9.3.1/lib/hazelcast-1.9.3.1.jar:../out/production/POC com.jteso.poc.superaudit.Processor 2
